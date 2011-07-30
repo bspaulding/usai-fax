0592a2d88cb154cb44a49654cd5656c344546d00
Bradley Spaulding <brad.spaulding@gmail.com>
2011-07-30 17:16:32 -0400

    Added test for FaxDocument.update_fax_documents.

--- test/unit/fax_document_test.rb
+++ test/unit/fax_document_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class FaxDocumentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "update_fax_documents should make faxes private by default" do
+    filepath = File.join(FAX_BASE_DIRECTORY, 'test.txt')
+    begin
+      File.open( filepath, 'w+') {|file| file.write "Hello, FaxDocumentTest!" }
+      assert_difference 'FaxDocument.count', 1 do
+        FaxDocument.update_fax_documents
+      end
+
+      assert FaxDocument.last.private
+    ensure
+      File.delete(filepath) if File.exists?(filepath)
+    end
   end
 end

a7b46110b214d49da2d33580798061d44e78275b
Bradley Spaulding <brad.spaulding@gmail.com>
2011-07-30 16:41:41 -0400

    Updated Rails to 2.3.11.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # A sample Gemfile
 source "http://rubygems.org"
 
-gem "rails",        "2.3.4"
+gem "rails",        "2.3.11"
 gem "authlogic",    "2.1.2"
 gem "sqlite3-ruby", "1.2.5"
 gem "rdoc",         ">=2.4.2"
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ FAX_BASE_DIRECTORY = "/Faxes" #"/Users/brads/Desktop/ruby_mv/private" #"/Library
 #FAX_PRIVATE_DIRECTORY = "/Library/Webserver/Documents/Private"
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
